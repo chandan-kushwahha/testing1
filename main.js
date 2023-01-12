6ab39f5bb49300f080aaa88f69c055673394eaae
chandan-kushwahha <120802499+chandan-kushwahha@users.noreply.github.com>
2023-01-12 18:02:36 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+  
+          function toggle(){
+            let btn = document.getElementById('btn')
+              var x = document.getElementById('show');
+              if(x.style.display === "none"){
+                  x.style.display ="block";
+                  
+              }else{
+                  x.style.display = "none";
+                  
+                  
+              }
+          }
+  
+          function Menu(){
+            let menuShow = document.getElementById('menu_show');
+              var x = document.getElementById('show');
+              
+              if(x.style.display === "none"){
+                  x.style.display ="block";
+               
+                  
+              }else{
+                  x.style.display = "none";
+                  
+              }
+          }
+  
